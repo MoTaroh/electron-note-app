6a519f94f4dedecc3196f2e1d8734579221edf7b
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-01-01 19:38:51 +0900

    Update: use transition to animation

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -3,7 +3,7 @@ import Editor from 'rich-markdown-editor'
 import base from 'rich-markdown-editor/dist/styles/theme'
 import Timer from './components/Timer'
 
-import { useSpring, animated } from 'react-spring'
+import { useSpring, useTransition, animated } from 'react-spring'
 
 const App: React.VFC = () => {
   const [isOpen, setIsOpen] = useState(true)
@@ -15,18 +15,21 @@ const App: React.VFC = () => {
     toolbarBackground: '#ffffff',
     toolbarItem: '#000000'
   }
-  const animationProps = useSpring({
-    opacity: !isOpen && isHover ? 1 : 0,
-    display: !isOpen && isHover ? 'block' : 'hidden',
-    x: !isOpen && isHover ? 0 : 32
-  })
-  const { height, opacity, padding } = useSpring({
-    from: { height: 0, opacity: 0, padding: 4 },
+  const { padding } = useSpring({
+    from: { padding: 4 },
     to: {
-      height: isOpen ? 600 : 0,
-      opacity: isOpen ? 1 : 0,
       padding: isOpen ? 12 : 4
     }
+  })
+  const hoverAnimation = useTransition(isHover, {
+    from: { width: 0, opacity: 0 },
+    enter: { width: 32, opacity: 1 },
+    leave: { width: 0, opacity: 0 }
+  })
+  const openHeightAnimation = useTransition(isOpen, {
+    from: { height: 0, opacity: 0 },
+    enter: { height: 600, opacity: 1 },
+    leave: { height: 0, opacity: 0 }
   })
 
   return (
@@ -49,27 +52,35 @@ const App: React.VFC = () => {
             →
           </button>
         )}
-        {!isOpen && isHover && (
-          <animated.button
-            style={animationProps}
-            onClick={() => setIsOpen(!isOpen)}
-            className="flex items-center justify-center w-8 text-white bg-black rounded bg-opacity-60"
-          >
-            ←
-          </animated.button>
+        {hoverAnimation((style, item) =>
+          item && !isOpen ? (
+            <animated.button
+              style={style}
+              onClick={() => setIsOpen(!isOpen)}
+              className="flex items-center justify-center text-white bg-black rounded bg-opacity-60"
+            >
+              ←
+            </animated.button>
+          ) : (
+            ''
+          )
         )}
         <Timer isOpen={isOpen}></Timer>
       </div>
-      {isOpen && (
-        <animated.div
-          className="flex flex-col space-y-4 w-[362px]"
-          style={{ opacity, height }}
-        >
-          <h3 className="text-lg font-semibold text-white ">NOTES</h3>
-          <div className="flex-1 px-8 py-4 bg-black rounded bg-opacity-60">
-            <Editor defaultValue="Hello, world!" theme={theme} />
-          </div>
-        </animated.div>
+      {openHeightAnimation((style, item) =>
+        item ? (
+          <animated.div
+            className="flex flex-col space-y-4 w-[362px]"
+            style={style}
+          >
+            <h3 className="text-lg font-semibold text-white ">NOTES</h3>
+            <div className="flex-1 px-8 py-4 bg-black rounded bg-opacity-60">
+              <Editor defaultValue="Hello, world!" theme={theme} />
+            </div>
+          </animated.div>
+        ) : (
+          ''
+        )
       )}
     </animated.div>
   )
